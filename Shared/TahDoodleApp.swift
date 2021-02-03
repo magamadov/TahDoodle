6ecbb4246efca623c86c163409b77e8da60dcf86
Zelimkhan Magamadov <magamadov@gmail.com>
2021-02-03 17:33:15 +0300

    Initial Commit

---
+++ Shared/TahDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahDoodleApp.swift
+//  Shared
+//
+//  Created by ZELIMKHAN MAGAMADOV on 03.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TahDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
