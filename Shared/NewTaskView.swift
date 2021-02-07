61342b474df51ada3673716dfeb6c604cbb0eb11
Zelimkhan Magamadov <magamadov@gmail.com>
2021-02-07 17:08:48 +0300

    Completed functionality for working on MacOS

---
+++ Shared/NewTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  NewTaskView.swift
+//  TahDoodle
+//
+//  Created by ZELIMKHAN MAGAMADOV on 07.02.2021.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTaskView()
+    }
+}
